6004b4666ff539a2ad88857138c08ff08fb16ab9
Mariana <a01702593@itesm.mx>
2021-11-19 11:31:56 -0600

    codigo comentado

--- CleaningSimulation/cellCleaning.py
+++ CleaningSimulation/cellCleaning.py
@@ -1,24 +1,34 @@
-from matplotlib.pyplot import grid
+
+
+"""Mesa allows the use of agents and models"""
 from mesa import Agent, Model
 from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
-from mesa.space import SingleGrid
 from mesa.datacollection import DataCollector
+
+"""To generate random numbers"""
 import random
+
+"""To facilitate numeric operations"""
 import numpy as np
 import pandas as pd
 
 def get_grid(model):
+    """Generates a copy of a grid"""
     grid = np.zeros( (model.agentGrid.width, model.agentGrid.height) )
     for (content, x, y) in model.agentGrid.coord_iter():
         grid[x][y] = model.dirtyGrid[x][y]
     return grid
 
 class CleaningAgent(Agent):
+    """Agent who cleans the dirtyGrid"""
     def __init__(self, unique_id, model):
+        """To initialize the agent with a unique_id and its respective model"""
         super().__init__(unique_id, model)
 
     def step(self):  
+        """Finds out possible directions in which the agent can move,
+        moves the agent to a random position and cleans the new cell"""
         possible_steps = self.model.agentGrid.get_neighborhood(
             self.pos,
             moore=True,
@@ -31,28 +41,35 @@ class CleaningAgent(Agent):
             self.model.dirty_cells -= 1
 
 class CleaningModel(Model):
+    """The cleaning model manages each agent. Provides an agent grid which contains the position
+     of each agent and a dirty grid which establishes if a cell is clean or dirty"""
     def __init__(self, width, height, agents, dirtyCells):
+        """To initialize grids, agents and a data collector"""
         self.num_agents = agents
         self.dirty_cells = dirtyCells
         self.agentGrid = MultiGrid(width, height, True)
         self.dirtyGrid = np.zeros( (self.agentGrid.width, self.agentGrid.height), dtype=float)
         self.schedule = SimultaneousActivation(self)
 
+        #Initializes all agents in the coordinate 1,1 of the agent grid
         for i in range (self.num_agents):
             a = CleaningAgent(i, self)
             self.agentGrid.place_agent(a, (1, 1))
             self.schedule.add(a)
 
-        i = self.dirty_cells-1
+        #Defines random cells as dirty                                 
+        i = self.dirty_cells-1   
         while(i >= 0):
             if self.dirtyGrid[random.randint(0,self.agentGrid.width-1)][random.randint(0,self.agentGrid.height-1)] == 0:
                 self.dirtyGrid[random.randint(0,self.agentGrid.width-1)][random.randint(0,self.agentGrid.height-1)] = 1
                 i = i - 1
 
+        #Initializes a data collector and assigns get_grid as the collecting function
         self.datacollector = DataCollector(model_reporters={"Grid": get_grid})
 
     def step(self):
-        self.datacollector.collect(self)
+        """Collects the data of each iteration and orders the agents to move"""
+        self.datacollector.collect(self)                                              
         self.schedule.step()
         if self.dirty_cells == 0:
             self.datacollector.collect(self)
--- CleaningSimulation/main.py
+++ CleaningSimulation/main.py
@@ -1,30 +1,36 @@
+"""To import the model in cellCleaning.py"""
 from cellCleaning import CleaningModel
 
+"""To animate the simulation"""
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 plt.rcParams["animation.html"] = "jshtml"
 matplotlib.rcParams['animation.embed_limit'] = 2**128
 
+"""To facilitate numeric operations"""
 import numpy as np
 import pandas as pd
 
-
+"""To measure compilation time"""
 import time
 import datetime
 
+"""Model constants"""
 M = 10
 N = 10
 AGENTS = 3
 DIRTY_PERCENTAGE = 9
 MAX_TIME = 2.0
 
+"""Start cronometer and initialize the model"""
 start_time = time.time()
 dirtyCells = int((DIRTY_PERCENTAGE/(M*N))*100)
 model = CleaningModel(M, N, AGENTS, dirtyCells)
 iterations = 0
 
-while dirtyCells > 0:
+"""Excecute a model step until there are no more dirty cells"""
+while dirtyCells > 0 and time.time() - start_time < MAX_TIME:
     model.step()
     dirtyCells = model.dirty_cells
     iterations += 1
@@ -33,6 +39,8 @@ print('Tiempo de ejecución:', str(datetime.timedelta(seconds=(time.time() - sta
 print('Porcentaje de celdas limpias:', ((M*N-dirtyCells)/(M*N))*100)
 print('Número de movimientos:', iterations*AGENTS)
 
+
+"""Model animation"""
 all_grid = model.datacollector.get_model_vars_dataframe()
 
 fig, axs = plt.subplots(figsize=(7,7))
